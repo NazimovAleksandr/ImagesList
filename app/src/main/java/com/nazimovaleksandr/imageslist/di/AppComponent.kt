de3a95fe4a400b01851edd72875a3d7239fed636
Aleksandr <50_Sanya@mail.ru>
2022-03-13 20:15:46 +0700

    Создал свой билдер @Component.Builder

--- app/src/main/java/com/nazimovaleksandr/imageslist/di/AppComponent.kt
+++ app/src/main/java/com/nazimovaleksandr/imageslist/di/AppComponent.kt
@@ -1,9 +1,19 @@
 package com.nazimovaleksandr.imageslist.di
 
+import android.content.Context
 import com.nazimovaleksandr.imageslist.presentation.screens.images_list.ImagesListFragment
+import dagger.BindsInstance
 import dagger.Component
 
-@Component(modules = [AppModule::class, DomainModule::class, DataModule::class])
+@Component(modules = [DomainModule::class, DataModule::class])
 interface AppComponent {
     fun injectFragmentImagesList(fragment: ImagesListFragment)
+
+    @Component.Builder
+    interface AppComponentBuilder {
+        @BindsInstance
+        fun context(context: Context): AppComponentBuilder
+
+        fun build(): AppComponent
+    }
 }
--- app/src/main/java/com/nazimovaleksandr/imageslist/presentation/app/App.kt
+++ app/src/main/java/com/nazimovaleksandr/imageslist/presentation/app/App.kt
@@ -2,12 +2,11 @@ package com.nazimovaleksandr.imageslist.presentation.app
 
 import android.app.Application
 import com.nazimovaleksandr.imageslist.di.AppComponent
-import com.nazimovaleksandr.imageslist.di.AppModule
 import com.nazimovaleksandr.imageslist.di.DaggerAppComponent
 
 class App : Application() {
     val appComponent: AppComponent =  DaggerAppComponent
         .builder()
-        .appModule(AppModule(this))
+        .context(this)
         .build()
 }
